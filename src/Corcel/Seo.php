68267ec5f4bd7a4e2a4b6b88fb4ff7ba07cfeee2
Marco Boom <marco@quest-media.nl>
2017-03-15 10:58:40 +0100

    Make SEO trait compatible with taxonomies

--- src/Corcel/Seo.php
+++ src/Corcel/Seo.php
@@ -2,6 +2,8 @@
 
 namespace WP4Laravel\Corcel;
 
+use Corcel\Options;
+
 trait Seo
 {
 
@@ -11,6 +13,10 @@ trait Seo
      */
     public function getSeoAttribute()
     {
+        if ($this instanceof \Corcel\TermTaxonomy) {
+            return $this->getSeoAttributeForTerms();
+        }
+
         return collect([
             'keywords' => $this->meta->getAttribute('_yoast_wpseo_focuskw') ?: '',
             'title' => $this->meta->getAttribute('_yoast_wpseo_title') ?: $this->title,
@@ -24,6 +30,30 @@ trait Seo
             'twitter-title' => $this->meta->getAttribute('_yoast_wpseo_twitter-title') ?: '',
             'twitter-description' => $this->meta->getAttribute('_yoast_wpseo_twitter-description') ?: $this->excerpt,
             'twitter-image' => $this->meta->getAttribute('_yoast_wpseo_twitter-image') ?: '',
+        ]);
+    }
+
+    protected function getSeoAttributeForTerms()
+    {
+        $meta = Options::get('wpseo_taxonomy_meta');
+
+        if (!empty($meta[$this->taxonomy][$this->term_id])) {
+            $data = collect($meta[$this->taxonomy][$this->term_id]);
+        }
+
+        return collect([
+            'keywords' => $this->meta->getAttribute('_yoast_wpseo_focuskw') ?: '',
+            'title' => $data['wpseo_title'] ?? $this->title,
+            'description' => $data['wpseo_title'] ?? $this->description,
+            'metakeywords' =>  '',
+            'noindex' => $data['wpseo_noindex'] ?? '',
+            'nofollow' => $data['wpseo_nofollow'] ?? '',
+            'opengraph-title' => $data['wpseo_opengraph-title'] ?? $this->title,
+            'opengraph-description' => $data['wpseo_opengraph-description'] ?? $this->description,
+            'image' => $data['wpseo_opengraph-image'] ?? '',
+            'twitter-title' => $data['wpseo_twitter-title'] ?? '',
+            'twitter-description' => $data['wpseo_twitter-description'] ?? $this->description,
+            'twitter-image' => $data['wpseo_twitter-image'] ?? '',
         ]);
     }
 }
