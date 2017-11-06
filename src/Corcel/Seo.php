a282a45db7b45231d5b2c9c3326c9c88c6e62e22
Jakob Buis <jakob.buis@in10.nl>
2017-11-06 15:44:54 +0100

    rename Seo keys to match meta names

--- src/Corcel/Seo.php
+++ src/Corcel/Seo.php
@@ -22,18 +22,17 @@ trait Seo
         });
 
         return collect([
-            'keywords' => $meta->get('_yoast_wpseo_focuskw') ?: '',
             'title' => $meta->get('_yoast_wpseo_title') ?: $this->title,
             'description' => $meta->get('_yoast_wpseo_metadesc') ?: $this->excerpt,
-            'metakeywords' => $meta->get('_yoast_wpseo_metakeywords') ?: '',
+            'keywords' => $meta->get('_yoast_wpseo_metakeywords') ?: '',
             'noindex' => $meta->get('_yoast_wpseo_meta-robots-noindex') ?: '',
             'nofollow' => $meta->get('_yoast_wpseo_meta-robots-nofollow') ?: '',
-            'opengraph-title' => $meta->get('_yoast_wpseo_opengraph-title') ?: $this->title,
-            'opengraph-description' => $meta->get('_yoast_wpseo_opengraph-description') ?: $this->excerpt,
-            'image' => $meta->get('_yoast_wpseo_opengraph-image') ?: '',
-            'twitter-title' => $meta->get('_yoast_wpseo_twitter-title') ?: '',
-            'twitter-description' => $meta->get('_yoast_wpseo_twitter-description') ?: $this->excerpt,
-            'twitter-image' => $meta->get('_yoast_wpseo_twitter-image') ?: '',
+            'og:title' => $meta->get('_yoast_wpseo_opengraph-title') ?: $this->title,
+            'og:site_name' => $meta->get('_yoast_wpseo_opengraph-description') ?: $this->excerpt,
+            'og:image' => $meta->get('_yoast_wpseo_opengraph-image') ?: '',
+            'twitter:title' => $meta->get('_yoast_wpseo_twitter-title') ?: '',
+            'twitter:description' => $meta->get('_yoast_wpseo_twitter-description') ?: $this->excerpt,
+            'twitter:image' => $meta->get('_yoast_wpseo_twitter-image') ?: '',
         ]);
     }
 
@@ -49,15 +48,14 @@ trait Seo
             'keywords' => $data['_yoast_wpseo_focuskw'] ?? '',
             'title' => $data['wpseo_title'] ?? $this->title,
             'description' => $data['wpseo_title'] ?? $this->description,
-            'metakeywords' =>  '',
             'noindex' => $data['wpseo_noindex'] ?? '',
             'nofollow' => $data['wpseo_nofollow'] ?? '',
-            'opengraph-title' => $data['wpseo_opengraph-title'] ?? $this->title,
-            'opengraph-description' => $data['wpseo_opengraph-description'] ?? $this->description,
-            'image' => $data['wpseo_opengraph-image'] ?? '',
-            'twitter-title' => $data['wpseo_twitter-title'] ?? '',
-            'twitter-description' => $data['wpseo_twitter-description'] ?? $this->description,
-            'twitter-image' => $data['wpseo_twitter-image'] ?? '',
+            'og:title' => $data['wpseo_opengraph-title'] ?? $this->title,
+            'og:description' => $data['wpseo_opengraph-description'] ?? $this->description,
+            'og:image' => $data['wpseo_opengraph-image'] ?? '',
+            'twitter:title' => $data['wpseo_twitter-title'] ?? '',
+            'twitter:description' => $data['wpseo_twitter-description'] ?? $this->description,
+            'twitter:image' => $data['wpseo_twitter-image'] ?? '',
         ]);
     }
 }
