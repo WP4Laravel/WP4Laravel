4ea38691fcb01ee3a86b81a2c9eb435890635df1
Jakob Buis <jakob.buis@in10.nl>
2018-02-06 14:10:31 +0100

    made ALT-attribute configurable for ImageFake

--- src/ImageFake.php
+++ src/ImageFake.php
@@ -12,21 +12,21 @@ use Illuminate\Support\Collection;
  *     'full' => '/build/imgs/test1-wide.png',
  *     '220w' => '/build/imgs/test1-wide.png',
  *     '440w' => '/build/imgs/test1-square.png',
- * ]);
+ * ]);dgisdhgosdhg
  */
 class ImageFake
 {
     /*
      * Construction
      */
-    public static function make(array $urls)
+    public static function make(array $urls, $alt = null)
     {
-        return new self($urls);
+        return new self($urls, $alt);
     }
 
     private $sizes;
 
-    private function __construct(array $urls)
+    private function __construct(array $urls, $alt)
     {
         $this->sizes = (new Collection($urls))->map(function ($size) {
             return ['url' => $size];
@@ -38,7 +38,7 @@ class ImageFake
                     'sizes' => $this->sizes->toArray(),
                 ]),
                 '_wp_attached_file' => $this->sizes->first()['url'],
-                '_wp_attachment_image_alt' => 'Some alt-text',
+                '_wp_attachment_image_alt' => $alt,
             ],
         ];
     }
