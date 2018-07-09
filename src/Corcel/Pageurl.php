3505aba08973c1b4503f41b1682889ed2c93ef08
Jakob Buis <jakob.buis@in10.nl>
2018-07-09 14:56:17 +0200

    code formatting

--- src/Corcel/Pageurl.php
+++ src/Corcel/Pageurl.php
@@ -10,51 +10,50 @@ trait Pageurl
     /**
      * Get the current page based on a
      * hierachel structure based on the url
-     * @param  	string $url
-     * @param	boolean $abort
+     * @param   string $url
+     * @param   boolean $abort
      * @return Page | null
      */
-    public static function url($url, $abort=true)
+    public static function url($url, $abort = true)
     {
         // Explode the url by slashes and reverse the collection
         $segments = collect(explode("/", $url))->reverse();
 
-        //	Remove the first part from the url segments
-        //	This will be the page to search as a starting point
+        //  Remove the first part from the url segments
+        //  This will be the page to search as a starting point
         $first = $segments->shift();
 
         // Get all pages with the slug based on the last segment
         $pages = static::slug($first)->published()->get();
 
-        //	Filter al found pages with the same slug
+        //  Filter al found pages with the same slug
         $selected = $pages->filter(function ($page) use ($segments) {
 
-            //	The current page has no parent
-            //	and there are not anymore segments
-            //	So the requested page, has to be this one
+            //  The current page has no parent
+            //  and there are not anymore segments
+            //  So the requested page, has to be this one
             if (!$page->post_parent && !$segments->count()) {
                 return true;
             }
 
-            //	The current page has a parent,
-            //	but there not anymore segments
-            //	So it cannot be this page
+            //  The current page has a parent,
+            //  but there not anymore segments
+            //  So it cannot be this page
             if ($page->post_parent && !$segments->count()) {
                 return false;
             }
 
-            //	Temp var to hold the current page in the hierarchy
+            //  Temp var to hold the current page in the hierarchy
             $current = $page;
 
-            //	Loop through the remaining url segments
+            //  Loop through the remaining url segments
             foreach ($segments as $segment) {
-
-                //	Set the current to the parent of the page
+                //  Set the current to the parent of the page
                 $current = $current->parent;
 
-                //	Check if de slug of the parent
-                //	is the same as the current segment
-                //	if not return false
+                //  Check if de slug of the parent
+                //  is the same as the current segment
+                //  if not return false
                 if (!$current || $current->slug != $segment) {
                     return false;
                 }
@@ -67,20 +66,20 @@ trait Pageurl
                 return true;
             }
 
-            //	No page foun
+            //  No page foun
             return false;
         });
-        //	Return the first item of the filtered collection
+        //  Return the first item of the filtered collection
         if ($return = $selected->first()) {
             return $return;
         }
 
-        //	If abort is true, send a 404
+        //  If abort is true, send a 404
         if ($abort) {
             abort(404);
         }
 
-        //	Otherwise just return null
+        //  Otherwise just return null
         return null;
     }
 
@@ -112,12 +111,12 @@ trait Pageurl
      */
     public static function homepage()
     {
-        //	Get the setting for the setted homepage
+        //  Get the setting for the setted homepage
         if (!$id = Option::get('page_on_front')) {
             return null;
         }
 
-        //	Find the page based on the id from the setting
+        //  Find the page based on the id from the setting
         if (!$page = static::find($id)) {
             return null;
         }
