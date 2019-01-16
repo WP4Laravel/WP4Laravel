f0caf0bd26d7e91857456c9b52f5b2a33201d695
Jakob Buis <jakob.buis@in10.nl>
2019-01-16 11:09:08 +0100

    remove empty constructor

--- src/Commands/WPURLReplace.php
+++ src/Commands/WPURLReplace.php
@@ -22,16 +22,6 @@ class WPURLReplace extends Command
     protected $description = 'Update hardcoded urls of other environments to the current';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
