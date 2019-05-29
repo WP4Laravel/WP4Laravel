20af3ff2cfbb36c0fdedcb6ec3359511665552ea
Bas de Beer <bas@dekantvandebeer.nl>
2019-05-29 17:00:36 +0200

    Updated routing

--- routes/api_v1.0.php
+++ routes/api_v1.0.php
@@ -4,7 +4,12 @@ Route::group([
     'prefix' => '{language}',
     'where' => ['language' => '(nl)']
 ], function () {
+    // Menulinks
+    Route::get('menulinks', 'Api\MenulinkController@index')->name('menulink_show');
+    // Exhibitions
     Route::get('exhibitions', 'Api\ExhibitionController@index')->name('exhibition_index');
     Route::get('exhibitions/{id}', 'Api\ExhibitionController@show')->name('exhibition_show');
     Route::get('exhibitions/{id}/download', 'Api\ExhibitionController@download')->name('exhibition_download');
+    // Artworks
+    Route::get('artworks/{id}', 'Api\ArtworkController@show')->name('artwork_show');
 });
