d8ce33cf34d1cac04a7c0d2ac735a9ea907fd1a1
Bas de Beer <bas@dekantvandebeer.nl>
2019-06-03 15:45:52 +0200

    Removed AcfFormatter

--- app/Http/Resources/Api/v1_0/BaseResource.php
+++ app/Http/Resources/Api/v1_0/BaseResource.php
@@ -3,12 +3,9 @@
 namespace App\Http\Resources\Api\v1_0;
 
 use Illuminate\Http\Resources\Json\Resource;
-use App\Http\Resources\Api\v1_0\Traits\AcfFormatter;
 
 class BaseResource extends Resource
 {
-    use AcfFormatter;
-
     /**
      * Transform the resource into an array.
      *
