01e5e18cc68dc47af15f9bfa9d18fb67ffc92a15
Bas de Beer <bas@dekantvandebeer.nl>
2019-06-05 16:41:34 +0200

    Updated deploy

--- deploy.php
+++ deploy.php
@@ -5,8 +5,8 @@ namespace Deployer;
 require_once __DIR__ . '/vendor/in10/deployment/recipes/in10.recipe.php';
 
 // Configuration
-set('application', '[PROJECTNAME]');
-set('repository', 'git@github.com:IN10/[PROJECT_GIT_NAME].git');
+set('application', 'Fotomuseum-API');
+set('repository', 'git@github.com:IN10/Fotomuseum-API.git');
 
 // Define stages
 host('test')
@@ -17,13 +17,13 @@ host('test')
     ->set('deploy_path', '~/fotomuseum-api.t05.in10projecten.nl');
 
 // host('acceptance')
-//   ->hostname('[host]')
+//   ->hostname('18.197.42.16')
 //   ->user('[username]')
 //   ->stage('acceptance')
 //   ->set('deploy_path', '~/www');
 
 // host('production')
-//   ->hostname('[host]')
+//   ->hostname('18.197.42.16')
 //   ->user('[username]')
 //   ->stage('production')
 //   ->set('deploy_path', '~/www');
