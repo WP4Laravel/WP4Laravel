5e2b189ed79c6119a776fc18cc8d19b71f0c23fd
remko janse <remko@in10.nl>
2019-11-15 15:30:40 +0100

    Correct upload path

--- resources/theme/library/config/storage.php
+++ resources/theme/library/config/storage.php
@@ -2,5 +2,5 @@
 
 //	Update the location of the uploads. Set to the public storage of wp4laravel
 
-update_option('upload_path', ABSPATH.'../storage');
+update_option('upload_path', WP_CONTENT_DIR . '/storage');
 update_option('upload_url_path', '/storage');
